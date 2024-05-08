84f2c9b35559794f28f5c1c791ac241a1405dc66
huijeong365 <sarahcho87@gmail.com>
2024-05-08 12:58:31 +0900

    update

--- shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +23,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -100,7 +109,10 @@ class ItemRepositoryTest {
             System.out.println(item.toString());
         }
     }
-    /*public void findByItemDetailTest(){
+    /*
+    @Test
+    @DisplayName("@nativeQuery를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
         for(Item item : itemList){
@@ -108,4 +120,20 @@ class ItemRepositoryTest {
         }
     }*/
 
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%"+"테스트 상품 상세 설명"+"%"))
+                .orderBy(qItem.price.desc());
+        List<Item> itemList = query.fetch();
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+
+    }
 }
